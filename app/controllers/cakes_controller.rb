df4db0df319c33eb7fa03dd90a70e52df413c9d8
nicolafeldman <nicola.feldman@yale.edu>
2014-07-08 14:10:10 -0400

    very small things

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -6,9 +6,9 @@ class CakesController < ApplicationController
   	@cake = current_user.cakes.build(flavor: "chocolate", 
   		message: "I made this for you! Hope you like it!", 
   		user_id: current_user.id, has_been_given: false, baked_by: current_user.name)
-  	  if @cake.save
+  	if @cake.save
   	  	flash[:success] = "Your Cake is Baked :)"
-		redirect_to current_user
+		    redirect_to current_user
 	  else
 	  	render 'static_pages/help'
 	  end
